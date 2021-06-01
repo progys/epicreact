62f1293fa6a800010c40c465253ed2dd2b1a6e01
progys <progys@gmail.com>
2021-06-02 00:30:47 +0300

    Remove comments

--- react-hooks/01.js
+++ react-hooks/01.js
@@ -4,11 +4,9 @@
 import * as React from 'react'
 
 function Greeting({initialName}) {
-  // 💣 delete this variable declaration and replace it with a React.useState call
   const [name, setName] = React.useState(initialName);
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
     setName(event.target.value);
   }
 
