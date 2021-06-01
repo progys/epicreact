e87d18af7793498cb658b898f8bc3d4903e2c8ca
progys <progys@gmail.com>
2021-06-01 23:48:40 +0300

    React controller input in form

---
+++ react-fundamentals/06.js
@@ -0,0 +1,43 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = React.useState()
+  const inputRef = React.useRef()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    onSubmitUsername(username)
+  }
+
+  const handleChange = e => {
+    setUsername(inputRef.current.value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={handleChange}
+          ref={inputRef}
+          id="username"
+          type="text"
+          value={username}
+        />
+      </div>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
