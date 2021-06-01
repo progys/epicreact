e570470bc60f2217d1ede84710d09ef3544b384a
progys <progys@gmail.com>
2021-06-02 01:06:10 +0300

    Add useEffect hook

---
+++ react-hooks/02.js
@@ -0,0 +1,38 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+
+function useLocalStorageState(key, initialValue){
+  const getInitialState = () => JSON.parse(window.localStorage.getItem(key)) || initialValue
+  const [value, setValue] = React.useState(getInitialState)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+   return [value, setValue];
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
