4690cc55625f15e591c6bb7e1bfbfa14713e143f
progys <progys@gmail.com>
2021-11-14 23:27:54 +0200

    use context exercise

---
+++ react-advanced-hooks/03.js
@@ -0,0 +1,43 @@
+// useContext: simple Counter
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+const CountContext = React.createContext()
+
+const useCount = () => {
+  const value = React.useContext(CountContext)
+  if (value === undefined) {
+    throw new Error('useCount must be used within a CountProvider.')
+  }
+  return value
+}
+
+const CountProvider = ({children}) => {
+  const value = React.useState(0)
+  return <CountContext.Provider value={value}>{children}</CountContext.Provider>
+}
+
+function CountDisplay() {
+  const [count] = useCount()
+  return <div>{`The current count is ${count}`}</div>
+}
+
+function Counter() {
+  const [, setCount] = useCount()
+  const increment = () => setCount(c => c + 1)
+  return <button onClick={increment}>Increment count</button>
+}
+
+function App() {
+  return (
+    <div>
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
+    </div>
+  )
+}
+
+export default App
